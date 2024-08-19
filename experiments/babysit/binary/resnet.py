1bf6b5a55b90fd9cc48103a18f8bca6ec4673b32
Miguel Martins <miguelmartins@Miguels-MacBook-Pro.local>
2024-08-19 15:39:23 +0100

    Remove color DA

--- experiments/babysit/binary/resnet.py
+++ experiments/babysit/binary/resnet.py
@@ -7,7 +7,7 @@ from keras.metrics import Precision, Recall, AUC, CategoricalAccuracy
 from keras.src.metrics import BinaryAccuracy
 
 from tensorflow.keras import backend as K
-from custom_models.cnns import simple_cnn_bn
+from custom_models.cnns import simple_cnn_bn, base_resnet50
 from custom_models.augmentation import basic_augmentation, basic_plus_color_augmentation
 from custom_models.optimization_utilities import get_standard_callbacks
 from etl.load_dataset import DatasetProcessor, get_tf_eggim_patch_dataset
@@ -37,7 +37,7 @@ def main():
                                                    internal_train_size=0.5,
                                                    random_state=42)
 
-    test_idx = 4
+    test_idx = 2
     print("FOLD ", test_idx)
     i = 0
     for df_train, df_val, df_test in split:
@@ -54,24 +54,20 @@ def main():
     # Convert the class weights dictionary to a tensor
     weights = tf.constant(list(class_weights_manual.values()), dtype=tf.float32)
 
-    tf_train_df = get_tf_eggim_patch_dataset(df_train, num_classes=n_classes, augmentation_fn=basic_plus_color_augmentation)
-    tf_val_df = get_tf_eggim_patch_dataset(df_val, num_classes=n_classes)
-    tf_test_df = get_tf_eggim_patch_dataset(df_test, num_classes=n_classes)
+    tf_train_df = get_tf_eggim_patch_dataset(df_train, num_classes=n_classes,
+                                             augmentation_fn=basic_plus_color_augmentation,
+                                             preprocess_fn=tf.keras.applications.resnet.preprocess_input)
+    tf_val_df = get_tf_eggim_patch_dataset(df_val, num_classes=n_classes,
+                                           preprocess_fn=tf.keras.applications.resnet.preprocess_input)
+    tf_test_df = get_tf_eggim_patch_dataset(df_test, num_classes=n_classes,
+                                            preprocess_fn=tf.keras.applications.resnet.preprocess_input)
 
     tf_train_df = tf_train_df.batch(batch_size)
     tf_val_df = tf_val_df.batch(batch_size)
     tf_test_df = tf_test_df.batch(batch_size)
 
 
-    model = simple_cnn_bn(input_shape=(224, 224, 3), n_classes=n_classes)
-    print(model.summary())
-    # Compile the model with Adam optimizer
-    # simple da
-    # 3/3 [==============================] - 0s 38ms/step - loss: 0.5910 - cat_accuracy: 0.7778 - precision: 0.7805 - recall: 0.7111 - auc: 0.9097
-    # color da
-    # 3/3 [==============================] - 0s 38ms/step - loss: 0.7556 - cat_accuracy: 0.7222 - precision: 0.7733 - recall: 0.6444 - auc: 0.8593
-    # no da
-    # 3/3 [==============================] - 0s 43ms/step - loss: 0.6302 - cat_accuracy: 0.7667 - precision: 0.7791 - recall: 0.7444 - auc: 0.9096
+    model = base_resnet50(input_shape=(224, 224, 3), n_classes=n_classes)
     model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=learning_rate),
                   loss=weighted_binary_crossentropy(class_weights_manual),
                   metrics=['accuracy', Precision(name='precision'), Recall(name='recall'),
